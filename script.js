690b0075eca6412713ba663f7882286439415404
Samprikta1234 <Sampriktabhattacharjee9@gmail.com>
2024-11-03 19:42:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Dark mode */
+body.dark-mode {
+    background-color: #181818;
+    color: #ddd;
+}
+
+body.dark-mode section {
+    background-color: rgba(40, 40, 40, 0.9);
+}
+
+@keyframes fadeIn {
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+}
